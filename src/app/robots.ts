fc6645b55f46b612b7bd12c2e5e731ee3677d683
Temuulen Bold <temka.school007@gmail.com>
2026-08-19 20:03:46 +0900

    Allow Google to crawl Next.js assets

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,5 +2,5 @@ import type { MetadataRoute } from "next";
 import { absoluteUrl, siteUrl } from "@/lib/site";
 
 export default function robots(): MetadataRoute.Robots {
-  return { rules: { userAgent: "*", allow: "/", disallow: ["/_next/"] }, sitemap: absoluteUrl("/sitemap.xml"), host: siteUrl };
+  return { rules: { userAgent: "*", allow: "/" }, sitemap: absoluteUrl("/sitemap.xml"), host: siteUrl };
 }
